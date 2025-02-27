cf9be0680e8c2141650360a66a815f3d7260cf54
Maher-alhomsy-se <maher.a@merkler00t.com>
2025-02-28 02:00:42 +0300

    install ioredis db

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from 'express';
 
 import cors from 'cors';
+import Redis from 'ioredis';
+
+const db = new Redis(
+  'rediss://default:AcNDAAIjcDE5ZDM1OGE1YjEyYjc0YWZiODllYmRmNGI5OTM3ZWZhMnAxMA@honest-snail-49987.upstash.io:6379'
+);
 
 const app = express();
 app.use(express.json());
@@ -8,6 +13,16 @@ app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello from express! ' });
+});
+
+app.get('/add-user', async (req, res) => {
+  try {
+    const result = await db.get('group_app', JSON.stringify({ name: 'Maher' }));
+
+    res.status(200).json({ message: 'OK', result });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal Server Error', error });
+  }
 });
 
 export default app;
