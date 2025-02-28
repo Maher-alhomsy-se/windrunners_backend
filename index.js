69a97d98584c98de0acf5dca2e5dacc1de06c39b
Maher-alhomsy-se <maher.a@merkler00t.com>
2025-02-28 03:09:52 +0300

    push

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 import cors from 'cors';
 import Redis from 'ioredis';
+import { ethers } from 'ethers';
 
 const db = new Redis(
   'rediss://default:AcNDAAIjcDE5ZDM1OGE1YjEyYjc0YWZiODllYmRmNGI5OTM3ZWZhMnAxMA@honest-snail-49987.upstash.io:6379'
@@ -11,17 +12,29 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
+const URL =
+  'https://base-mainnet.infura.io/v3/76d6ec90a58e4984adea4d341e6b8de7';
+
+const provider = new ethers.JsonRpcProvider(URL);
+
+app.get('/', async (req, res) => {
+  const blockNumber = await provider.getBlockNumber();
+
+  console.log('Block number: ' + blockNumber);
+
   res.status(200).json({ message: 'Hello from express! ' });
 });
 
-app.get('/add-user', async (req, res) => {
+app.post('/verify', async (req, res) => {
+  const { tx } = req.body;
   try {
-    const result = await db.set('group_app', JSON.stringify({ name: 'Maher' }));
+    const transaction = await provider.getTransaction(tx.hash);
 
-    res.status(200).json({ message: 'OK', result });
+    return res
+      .status(200)
+      .json({ message: 'Success!', data: JSON.stringify(transaction) });
   } catch (error) {
-    res.status(500).json({ message: 'Internal Server Error', error });
+    return res.status(500).json({ message: 'Internal Server Error !' });
   }
 });
 
