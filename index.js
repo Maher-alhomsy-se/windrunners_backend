452d2a4c54d0938a132c74057459c2939ff0293b
Maher-alhomsy-se <maher.a@merkler00t.com>
2025-02-28 07:33:55 +0300

    remove throw an error

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ app.get('/', async (req, res) => {
   console.log(receipt);
 
   try {
-    throw new Error('Transaction');
-    // res.status(200).json({ message: 'Hello from Express! ' });
+    // throw new Error('Transaction');
+    res.status(200).json({ message: 'Hello from Express! ' });
   } catch (error) {
     res.status(500).json({ message: 'Error: ' + error.message });
   }
