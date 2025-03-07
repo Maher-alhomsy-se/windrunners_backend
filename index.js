f2d5895c5c0991482840af4719363a31352c86fe
Maher-alhomsy-se <maher.a@merkler00t.com>
2025-03-07 20:55:29 +0300

    update code

--- index.js
+++ index.js
@@ -5,43 +5,23 @@ import Redis from 'ioredis';
 import { ethers, formatEther } from 'ethers';
 import TelegramBot from 'node-telegram-bot-api';
 
-const bot = new TelegramBot('7992828654:AAGCEaVx1OxZA6Em79ow9P1gP0KE0SB7Mnw', {
-  polling: true,
-});
+const CHANNEL_ID = '-1002282561796';
+const ONE_MONTH_MS = 30 * 24 * 60 * 60 * 1000;
 
-const db = new Redis(
-  'rediss://default:AcNDAAIjcDE5ZDM1OGE1YjEyYjc0YWZiODllYmRmNGI5OTM3ZWZhMnAxMA@honest-snail-49987.upstash.io:6379'
-);
+const URL =
+  'https://base-mainnet.infura.io/v3/76d6ec90a58e4984adea4d341e6b8de7';
+const BOT_TOKEN = '7992828654:AAGCEaVx1OxZA6Em79ow9P1gP0KE0SB7Mnw';
+const DB_URL =
+  'rediss://default:AcNDAAIjcDE5ZDM1OGE1YjEyYjc0YWZiODllYmRmNGI5OTM3ZWZhMnAxMA@honest-snail-49987.upstash.io:6379';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const URL =
-  'https://base-mainnet.infura.io/v3/76d6ec90a58e4984adea4d341e6b8de7';
-
+const db = new Redis(DB_URL);
 const provider = new ethers.JsonRpcProvider(URL);
-
-const GROUP_ID = '-1002415386979';
-const ONE_MONTH_MS = 30 * 24 * 60 * 60 * 1000;
-const ADDRESS = '0xa8ed9b14658Bb9ea3e9CC1e32BA08fcbe6888927';
-
-app.get('/', async (req, res) => {
-  const hash =
-    '0x5754a2ddf5085c85393cbf1395db251e97b2ea1f7d28515710c4777abb233e80';
-
-  const receipt = await provider.getTransactionReceipt(hash);
-
-  console.log(receipt);
-
-  try {
-    // throw new Error('Transaction');
-    res.status(200).json({ message: 'Hello from Express! ' });
-  } catch (error) {
-    res.status(500).json({ message: 'Error: ' + error.message });
-  }
-});
+const bot = new TelegramBot(BOT_TOKEN, { polling: true });
 
 app.post('/verify', async (req, res) => {
   const { tx, userId, address } = req.body;
@@ -59,11 +39,21 @@ app.post('/verify', async (req, res) => {
     const key = `group:${address}:app`;
     const data = { etherValue, address, date: Date.now() + ONE_MONTH_MS };
 
-    db.set(key, JSON.stringify(data));
-
     if (etherValue === '0.001805') {
-      const isApproved = await bot.approveChatJoinRequest(GROUP_ID, userId);
-      console.log(isApproved);
+      db.set(key, JSON.stringify(data));
+
+      const inviteLink = await bot.createChatInviteLink(CHANNEL_ID, {
+        expire_date: Math.floor(Date.now() / 1000) + 300,
+        member_limit: 1,
+      });
+
+      const { chat } = await bot.sendMessage(
+        userId,
+        `✅ Approved! Join here: [Click to Join](${inviteLink.invite_link})`,
+        { parse_mode: 'Markdown' }
+      );
+
+      console.log(`send link to : ${chat.first_name || chat.id}`);
     }
 
     return res
@@ -86,7 +76,9 @@ setInterval(async () => {
 
     if (expireTime && Date.now() >= expireTime) {
       try {
-        await bot.unbanChatMember(GROUP_ID, userId, { only_if_banned: false });
+        await bot.unbanChatMember(CHANNEL_ID, userId, {
+          only_if_banned: false,
+        });
         await db.del(`group:${address}:app`);
         console.log(`❌ User ${address} removed from the group after 1 month.`);
       } catch (err) {
